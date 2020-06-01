b5e1ddfc573dcd35cb80ad611b10c350287a5688
Jonathan C MacMillan <jmacmill@gmail.com>
2020-06-01 13:06:36 +1000

    ttt first commit - connected all the files

---
+++ js/main.js
@@ -0,0 +1,37 @@
+console.log('tick tack toe motherfuckers!')
+
+
+$('button').click(function(){
+  return this.attr("id");
+})
+
+
+const player1 = {
+  choices: [],
+
+}
+
+
+
+
+
+
+
+
+
+
+
+const currentChoice = function(letter, player){
+  player.choices.push(letter);
+}
+
+
+
+
+
+
+
+
+
+
+//
