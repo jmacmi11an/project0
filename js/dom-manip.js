4844a0caf014341c53525d9e5fbac8f32c33035b
Jonathan C MacMillan <jmacmill@gmail.com>
2020-06-01 14:03:07 +1000

    test

---
+++ js/dom-manip.js
@@ -0,0 +1,8 @@
+console.log("I'm here too bitches!")
+
+
+
+
+$('button').click(function(){     //first attempt to get the id string from a button press
+  return this.attr("id");
+})
--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 console.log('tick tack toe motherfuckers!')
 
 
-$('button').click(function(){
-  return this.attr("id");
-})
+const win = [
+  ["a", "b", "c"],
+  ["d", "e", "f"],
+  ["g", "h", "i"],
+  ["a", "d", "g"],
+  ["b", "e", "h"],
+  ["c", "f", "i"],
+  ["a", "e", "i"],
+  ["c", "e", "g"],
+];
+
+const win = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+]
+
 
 
 const player1 = {
@@ -16,18 +35,27 @@ const player1 = {
 
 
 
-
-
-
-
-
 const currentChoice = function(letter, player){
   player.choices.push(letter);
+}
+
+const sortedChoices = function(array){
+  array.sort;
+}
+
+const determineWin = function(array){
+  if (array.length >= 3){
+    for (let i = 0; i < win.length; i++) {
+      if (sortedArray()[i] === )
+    }
+  }
 }
 
 
 
 
+
+console.log(currentChoice("", player1))
 
 
 
