a4aa339313c568ef6431a7f83d307d58854b4238
Jonathan C MacMillan <jmacmill@gmail.com>
2020-06-01 17:19:02 +1000

    still plugging away

--- js/dom-manip.js
+++ js/dom-manip.js
@@ -1,8 +1,108 @@
 console.log("I'm here too bitches!")
 
+const win = [
+  ["abc"],
+  ["def"],
+  ["ghi"],
+  ["adg"],
+  ["beh"],
+  ["cfi"],
+  ["aei"],
+  ["ceg"],
+];
+const boxes = {
+  a: 0,
+  b: 0,
+  c: 0,
+  d: 0,
+  e: 0,
+  f: 0,
+  g: 0,
+  h: 0,
+  i: 0
+}; // all possible guesses
+
+
+let firstPlayer = [];  //first player guesses
+let secondPlayer = []; //second player guesses
+
+let playerTurn = true;    //this just makes sure each new choice is a new player
+
+
+const isEmpty = function(string){
+  if (boxes.string !== 0){
+    return false;
+  } else {
+    boxes.string = 1;
+    return true;
+  }
+}
+
+
+// const currentChoice = function(letter, player){ //
+//   player.choices.push(letter);
+// }
 
 
 
-$('button').click(function(){     //first attempt to get the id string from a button press
-  return this.attr("id");
+
+
+// const sortedChoices = function(array){ //this returns an array of alphabetized strings
+//   return array.sort();
+// }
+
+
+
+//when one of the 9 buttons is pressed
+
+
+
+
+$('button').click(function(){
+  let box = $(this).attr("id");               // box is now a string of a character a-i
+  if (isEmpty(box)){                          // check if the box has been ticked yet.
+    if (playerTurn){                          // check to see who's turn
+      firstPlayer.push(box);                  // add the square to the player array
+      determineWin(firstPlayer);              // run the win check function
+      playerTurn = false;                     // change who's turn it is
+    } else {
+      secondPlayer.push(box);
+      determineWin(secondPlayer);
+      playerTurn = true;
+    }
+    console.log(`first player choices ${firstPlayer}, second player choices ${secondPlayer}.`)
+  };
+
+
+    console.log("You've already checked that box")
 })
+
+
+
+const determineWin = function(array){
+  array.sort();                            //alphabetize the array
+  array.toString("");                      //turn the array into a string
+  if (array.length === 3){                    //make sure array is long enough
+    for (let i = 0; i < win.length; i++) {          //iterate across the "win" array
+      if (array !== win[i]){
+        console.log("keep playing");
+      } else {
+        console.log(`you win! ${firstPlayer}: ${secondPlayer}`);
+      }
+    }
+  }
+}
+
+
+
+// //add event listeners
+// $('button').click(function(){     //first attempt to get the id string from a button press
+//   if (playerTurn){
+//     firstPlayer.push($(this).attr("id"));
+//     playerTurn = false;
+//   } else {
+//     secondPlayer.push($(this).attr("id"));
+//     playerTurn = true;
+//   }
+//   alert (`first player choices ${firstPlayer}, second player choices ${secondPlayer}.`)
+// })
--- js/main.js
+++ js/main.js
@@ -1,65 +1,100 @@
 console.log('tick tack toe motherfuckers!')
 
 
-const win = [
-  ["a", "b", "c"],
-  ["d", "e", "f"],
-  ["g", "h", "i"],
-  ["a", "d", "g"],
-  ["b", "e", "h"],
-  ["c", "f", "i"],
-  ["a", "e", "i"],
-  ["c", "e", "g"],
-];
-
-const win = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [1, 4, 7],
-  [2, 5, 8],
-  [3, 6, 9],
-  [1, 5, 9],
-  [3, 5, 7]
-]
-
-
-
-const player1 = {
-  choices: [],
-
-}
+// const win = [
+//   ["abc"],
+//   ["def"],
+//   ["ghi"],
+//   ["adg"],
+//   ["beh"],
+//   ["cfi"],
+//   ["aei"],
+//   ["ceg"],
+// ];
+//
+//
+// const win = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [3, 6, 9],
+//   [1, 5, 9],
+//   [3, 5, 7]
+// ]
 
 
-
-
-
-
-const currentChoice = function(letter, player){
-  player.choices.push(letter);
-}
-
-const sortedChoices = function(array){
-  array.sort;
-}
-
-const determineWin = function(array){
-  if (array.length >= 3){
-    for (let i = 0; i < win.length; i++) {
-      if (sortedArray()[i] === )
-    }
-  }
-}
+//
+// const player1 = {
+//   index1: 0,
+//   index2: 0,
+// }
 
 
 
 
 
-console.log(currentChoice("", player1))
 
 
 
 
 
+
+
+//
+//
+// if ((firstChoice[i] === secondChoice[i]) && (firstChoice[i] === thirdChoice[i]){
+//   alert("you win");
+// } else {
+//   if ((firstChoice[i] === firstChoice[i+ 1]) && (secondChoice[i] === secondChoice[i+1]) && (thirdChoice[i] === thirdChoice[i+1)){
+//     alert("you win");
+//   }
+// }
+
+
+
+//
+// const currentChoice = function(letter, player){
+//   player.choices.push(letter);
+// }
+//
+// const sortedChoices = function(array){ //this returns an array of alphabetized strings
+//   return array.sort();
+// }
+
+
+// const determineWin = function(array){
+//   if (array.length === 3){
+//     for (let i = 0; i < win.length; i++) {
+//       let possibleWin = array[i];
+//       for (let j = 0; j < possibleWin.length; j++){
+//         if (sortedArray()[j] === possibleWin[j]){
+//         alert(you win!)
+//         }
+//       }
+//     }
+//   }
+// }
+
+
+
+
+
+// console.log(currentChoice("", player1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+//outliers - solve for somebody picking a square that has been picked already;
 
 //
