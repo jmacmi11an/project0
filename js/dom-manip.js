8ce88e68dd6218a3b5537a503e36d7eebab2f19c
Jonathan C MacMillan <jmacmill@gmail.com>
2020-06-02 13:45:56 +1000

    the logic works, and there is a basic and functional css and html

--- js/dom-manip.js
+++ js/dom-manip.js
@@ -1,14 +1,15 @@
-
-const win = [
-  "abc",
-  "def",
-  "ghi",
-  "adg",
-  "beh",
-  "cfi",
-  "aei",
-  "ceg"
-];
+const resetArrays = function(){
+  return [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"],
+    ["a", "d", "g"],
+    ["b", "e", "h"],
+    ["c", "f", "i"],
+    ["a", "e", "i"],
+    ["c", "e", "g"],
+  ];
+}  //this resets the playerArrays for a new game
 let boxes = {
   a: 0,
   b: 0,
@@ -18,67 +19,29 @@ let boxes = {
   f: 0,
   g: 0,
   h: 0,
-  i: 0
-}; // all possible guesses
+  i: 0,
+  count: 0                    // all possible guesses
+};
 
+let player1Win = resetArrays();
+let player2Win = resetArrays();
+let playerTurn = true;            //this just makes sure each new choice is a new player.
 
-let firstPlayer = [];  //first player guesses
-let secondPlayer = []; //second player guesses
-
-let playerTurn = true;    //this just makes sure each new choice is a new player
-
-
-//tested and works
+//tested and works - this makes sure the box is empty before it's altered.
 const isEmpty = function(string){
   if (boxes[string] !== 0){
     return false;
   } else {
     boxes[string] = 1;
+    boxes.count+= 1;
     return true;
   }
 }
 
-
-// const currentChoice = function(letter, player){ //
-//   player.choices.push(letter);
-// }
-
-
-
-// const sortedChoices = function(array){ //this returns an array of alphabetized strings
-//   return array.sort();
-// }
-
-
-
-//when one of the 9 buttons is pressed
-$('button').click(function(){
-  let box = $(this).attr("id");               // box is now a string of a character a-i
-
-  if (isEmpty(box)){                          // check if the box has been ticked yet.
-    if (playerTurn){                          // check to see who's turn
-      firstPlayer.push(box);                  // add the square to the player array
-      determineWin(firstPlayer);              // run the win check function
-      playerTurn = false;                     // change who's turn it is
-    } else {
-      secondPlayer.push(box);
-      determineWin(secondPlayer);
-      playerTurn = true;
-    }
-  } else {
-    if (isEmpty(box) === false){
-      console.log("you already selected that box");
-    }
-  }
-
-})
-
-
-
-// Need to write a win function that resets the game and possibly does other shit.
-const endGame = function(){
-  firstPlayer = [];
-  secondPlayer = [];
+//this is a function that runs after there is a win or draw
+const reset = function(){
+  player1Win = resetArrays();
+  player2Win = resetArrays();
   playerTurn = true;
   boxes = {
     a: 0,
@@ -89,41 +52,64 @@ const endGame = function(){
     f: 0,
     g: 0,
     h: 0,
-    i: 0
+    i: 0,
+    count: 0
   };
+  $('img').removeClass();
+  morty = true;
 }
 
-
-
-
-
-//Still need to solve for if the game goes to 4 or 5 choices.
-const determineWin = function(array){
-  let sorted = array.sort();                            //alphabetize the array
-  let playerString = sorted.join("")                   //turn the array into a string
-  if (playerString.length === 3){                    //make sure array is long enough
-    for (let i = 0; i < win.length; i++) {          //iterate across the "win" array
-      if (playerString === win[i]){
-        endGame();
-        console.log(`you win! ${firstPlayer}: ${secondPlayer}`);
+// this is a function that determines if a player has won
+const determineWin = function(array, box){
+  for (let i = 0; i <array.length; i++){          //iterate across the winArray
+    let winArray = array[i];                        //define a single possible win path
+    for (let j = 0; j < winArray.length; j++){      //iterate across the win path
+      if (winArray[j] === box){                     // check if most recent box was in win path
+        winArray.splice(j, 1);                    // if so, delete ticked box from win path
+        if (winArray.length === 0){               // if the winpath is empty that triggers a win.
+          alert("you win!");
+          reset();
+        }
       }
     }
   }
-}
+};
+
+
+
+$('img').click(function(){
+  let box = $(this).attr("id");               // box is now a string of a character a-i
+  if (isEmpty(box)){                          // check if the box has been ticked yet.
+    if (playerTurn){                          // check to see who's turn
+      $(this).addClass('morty');
+      determineWin(player1Win, box);             // run the win check function
+      playerTurn = false;                     // change who's turn it is
+    } else {
+      $(this).addClass('rick');
+      determineWin(player2Win, box);
+      playerTurn = true;
+    }
+  } else {
+    if (isEmpty(box) === false){
+      console.log("you already selected that box");
+    }
+  }
+  if (boxes.count === 9){
+    alert("it's a draw");
+    reset();
+  }
+});
 
 
 
 
 
 
-// //add event listeners
-// $('button').click(function(){     //first attempt to get the id string from a button press
-//   if (playerTurn){
-//     firstPlayer.push($(this).attr("id"));
-//     playerTurn = false;
-//   } else {
-//     secondPlayer.push($(this).attr("id"));
-//     playerTurn = true;
-//   }
-//   alert (`first player choices ${firstPlayer}, second player choices ${secondPlayer}.`)
-// })
+
+
+
+
+
+
+
+//
