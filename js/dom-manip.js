29a623ac7255eb062a9726317c5d05b7dcab4b0f
Jonathan C MacMillan <jmacmill@gmail.com>
2020-06-02 22:31:20 +1000

    a complete, resettable, and primitively styled version

--- js/dom-manip.js
+++ js/dom-manip.js
@@ -1,115 +1,56 @@
-const resetArrays = function(){
-  return [
-    ["a", "b", "c"],
-    ["d", "e", "f"],
-    ["g", "h", "i"],
-    ["a", "d", "g"],
-    ["b", "e", "h"],
-    ["c", "f", "i"],
-    ["a", "e", "i"],
-    ["c", "e", "g"],
-  ];
-}  //this resets the playerArrays for a new game
-let boxes = {
-  a: 0,
-  b: 0,
-  c: 0,
-  d: 0,
-  e: 0,
-  f: 0,
-  g: 0,
-  h: 0,
-  i: 0,
-  count: 0                    // all possible guesses
-};
+$(document).ready(function(){
 
-let player1Win = resetArrays();
-let player2Win = resetArrays();
-let playerTurn = true;            //this just makes sure each new choice is a new player.
 
-//tested and works - this makes sure the box is empty before it's altered.
-const isEmpty = function(string){
-  if (boxes[string] !== 0){
-    return false;
-  } else {
-    boxes[string] = 1;
-    boxes.count+= 1;
-    return true;
-  }
-}
+// $('.second-player').hide();
 
-//this is a function that runs after there is a win or draw
-const reset = function(){
-  player1Win = resetArrays();
-  player2Win = resetArrays();
-  playerTurn = true;
-  boxes = {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0,
-    h: 0,
-    i: 0,
-    count: 0
-  };
-  $('img').removeClass();
-  morty = true;
-}
+// the game starts:
+// button visible
+// paragraphs invisible
+// #morty class first player
+// rick class second players
+// images invisible
 
-// this is a function that determines if a player has won
-const determineWin = function(array, box){
-  for (let i = 0; i <array.length; i++){          //iterate across the winArray
-    let winArray = array[i];                        //define a single possible win path
-    for (let j = 0; j < winArray.length; j++){      //iterate across the win path
-      if (winArray[j] === box){                     // check if most recent box was in win path
-        winArray.splice(j, 1);                    // if so, delete ticked box from win path
-        if (winArray.length === 0){               // if the winpath is empty that triggers a win.
-          alert("you win!");
-          reset();
-        }
+$('button').on('click', function(){
+  $(this).css('visibility', 'hidden');
+  $('p').css('visibility', 'visible');
+  $('p#morty').css('visibility', 'visible').show();
+  $('p.second-player').hide();
+  $('img').css('visibility', 'visible');
+})
+
+
+
+//this goes in the below
+// $('.first-player, .second-player').toggle(200);
+
+
+  $('img').click(function(){
+    let box = $(this).attr("id");                     // box is now a string of a character a-i
+    if (isEmpty(box)){                                 // check if the box has been ticked yet.
+      $('.first-player, .second-player').toggle(200);         //NEW TEST
+      if (playerTurn){                                  // See if it's Morty's turn
+        $(this).addClass('morty');
+        playerTurn = false;                             // change who's turn it is
+        determineWin(player1Win, box);                  // run the win check function
+      } else {
+        $(this).addClass('rick');
+        determineWin(player2Win, box);
+        playerTurn = true;
+      }
+    } else {
+      if (isEmpty(box) === false){
+        console.log("you already selected that box");
       }
     }
-  }
-};
-
-
-
-$('img').click(function(){
-  let box = $(this).attr("id");               // box is now a string of a character a-i
-  if (isEmpty(box)){                          // check if the box has been ticked yet.
-    if (playerTurn){                          // check to see who's turn
-      $(this).addClass('morty');
-      determineWin(player1Win, box);             // run the win check function
-      playerTurn = false;                     // change who's turn it is
-    } else {
-      $(this).addClass('rick');
-      determineWin(player2Win, box);
-      playerTurn = true;
+    if (boxes.count === 9){
+      alert("it's a draw");
+      reset();
     }
-  } else {
-    if (isEmpty(box) === false){
-      console.log("you already selected that box");
-    }
-  }
-  if (boxes.count === 9){
-    alert("it's a draw");
-    reset();
-  }
+  });
+
+
+
+
+
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
--- js/main.js
+++ js/main.js
@@ -1,98 +1,81 @@
+const resetArrays = function(){
+  return [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"],
+    ["a", "d", "g"],
+    ["b", "e", "h"],
+    ["c", "f", "i"],
+    ["a", "e", "i"],
+    ["c", "e", "g"],
+  ];
+}  //this resets the playerArrays for a new game
+let boxes = {
+  a: 0,
+  b: 0,
+  c: 0,
+  d: 0,
+  e: 0,
+  f: 0,
+  g: 0,
+  h: 0,
+  i: 0,
+  count: 0                    // all possible guesses
+};
 
-// const win = [
-//   ["abc"],
-//   ["def"],
-//   ["ghi"],
-//   ["adg"],
-//   ["beh"],
-//   ["cfi"],
-//   ["aei"],
-//   ["ceg"],
-// ];
-//
-//
-// const win = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-//   [7, 8, 9],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [3, 6, 9],
-//   [1, 5, 9],
-//   [3, 5, 7]
-// ]
+let player1Win = resetArrays();
+let player2Win = resetArrays();
+let playerTurn = true;            //this just makes sure each new choice is a new player.
 
+//tested and works - this makes sure the box is empty before it's altered.
+const isEmpty = function(string){
+  if (boxes[string] !== 0){
+    return false;
+  } else {
+    boxes[string] = 1;
+    boxes.count+= 1;
+    return true;
+  }
+}
 
-//
-// const player1 = {
-//   index1: 0,
-//   index2: 0,
-// }
+//this is a function that runs after there is a win or draw
+const reset = function(){
+  player1Win = resetArrays();
+  player2Win = resetArrays();
+  playerTurn = true;
+  boxes = {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0,
+    g: 0,
+    h: 0,
+    i: 0,
+    count: 0
+  };
+  $('button').css('visibility', 'visible');
+  $('img').removeClass('morty').removeClass('rick').css('visibility', 'hidden');
+  // $('div p').css('visibility', 'hidden');
+  $('p#morty').removeClass('first-player').removeClass('second-player').addClass('first-player').hide();
+  $('p#rick').removeClass('first-player').removeClass('second-player').addClass('second-player').hide();
+};
 
+// this is a function that determines if a player has won
+const determineWin = function(array, box){
+  for (let i = 0; i <array.length; i++){          //iterate across the winArray
+    let winArray = array[i];                        //define a single possible win path
+    for (let j = 0; j < winArray.length; j++){      //iterate across the win path
+      if (winArray[j] === box){                     // check if most recent box was in win path
+        winArray.splice(j, 1);                    // if so, delete ticked box from win path
+        if (winArray.length === 0){               // if the winpath is empty that triggers a win.
+          alert("you win!");
+          reset();
+        }
+      }
+    }
+  }
+};
 
-
-
-
-
-
-
-
-
-
-
-//
-//
-// if ((firstChoice[i] === secondChoice[i]) && (firstChoice[i] === thirdChoice[i]){
-//   alert("you win");
-// } else {
-//   if ((firstChoice[i] === firstChoice[i+ 1]) && (secondChoice[i] === secondChoice[i+1]) && (thirdChoice[i] === thirdChoice[i+1)){
-//     alert("you win");
-//   }
-// }
-
-
-
-//
-// const currentChoice = function(letter, player){
-//   player.choices.push(letter);
-// }
-//
-// const sortedChoices = function(array){ //this returns an array of alphabetized strings
-//   return array.sort();
-// }
-
-
-// const determineWin = function(array){
-//   if (array.length === 3){
-//     for (let i = 0; i < win.length; i++) {
-//       let possibleWin = array[i];
-//       for (let j = 0; j < possibleWin.length; j++){
-//         if (sortedArray()[j] === possibleWin[j]){
-//         alert(you win!)
-//         }
-//       }
-//     }
-//   }
-// }
-
-
-
-
-
-// console.log(currentChoice("", player1))
-
-
-
-
-
-
-
-
-
-
-
-
-
-//outliers - solve for somebody picking a square that has been picked already;
-
-//
+//note for tomorrow - you may want to always reset and alert outside of the above function and inside of the dom function
